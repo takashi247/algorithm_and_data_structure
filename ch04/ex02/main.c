9ee46d76976ce02ae4b17ade4469449cf4a9c4c0
takashi247 <takashi247@gmail.com>
2021-08-28 22:14:00 +0900

    modify ex01 and add ex02

--- ch04/ex02/main.c
+++ ch04/ex02/main.c
@@ -1,9 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+
+#define MAX_LEN 100
 
 long long
 	get_tribo(int n)
 {
+	static long long	memo[MAX_LEN];
+
 	if (n == 0)
 		return (0);
 	else if (n == 1)
@@ -11,7 +16,11 @@ long long
 	else if (n == 2)
 		return (1);
 	else
-		return (get_tribo(n - 1) + get_tribo(n - 2) + get_tribo(n - 3));
+	{
+		if (memo[n] == 0)
+			memo[n] = get_tribo(n - 1) + get_tribo(n - 2) + get_tribo(n - 3);
+		return (memo[n]);
+	}
 }
 
 int
